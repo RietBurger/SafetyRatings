4a204faaa8cd516e0b3f514ff5894eccc942ed9f
RietBurger <empoweryourself36@gmail.com>
2021-05-30 12:00:27 +0200

    Works with selection of Places

--- SafetyRatings/Controllers/SafetyController.cs
+++ SafetyRatings/Controllers/SafetyController.cs
@@ -26,13 +26,15 @@ namespace SafetyRatings.Controllers
             };
 
             // oAuth Token
-            var token = "FGMfVJGub3oSOsVMzFCRAgRCFjGT";
+            var token = "GnKcGPeEXWsfUq5uySZA7Q6oe3CT";
+            var latitude = "41.397158";
+            var longitude = "2.160873";
 
             // see if token is still valid. if not, get new token
             token = Check_token_validity(ref token);
 
             // Call API and return response
-            IRestResponse response = get_info(ref token);
+            IRestResponse response = get_info(ref token, ref latitude, ref longitude);
 
             int statusCode = (int)response.StatusCode;
             Debug.WriteLine(statusCode);
@@ -41,7 +43,6 @@ namespace SafetyRatings.Controllers
 
             if (statusCode == 200)
             {
-                int i = (int)jObject["meta"]["count"];
                 int count = 0;
                 int maxScore = 100; // 100 is most dangerous, 0 is safest
                 int maxScoreId = 0;
@@ -59,46 +60,50 @@ namespace SafetyRatings.Controllers
                 //string[] PlaceName = enumerable;
                 //string[] Scores;
 
-                foreach (var item in jObject["data"])
+                if (jObject["data"] != null)
                 {
-                    Debug.WriteLine(count);
-                    currScore = int.Parse(jObject["data"][count]["safetyScores"][fetchScore].ToString());
-                    if (currScore < maxScore)
+
+                    foreach (var item in jObject["data"])
                     {
-                        maxScore = currScore;
-                        maxScoreId = count;
+                        Debug.WriteLine(count);
+                        currScore = int.Parse(jObject["data"][count]["safetyScores"][fetchScore].ToString());
+                        if (currScore < maxScore)
+                        {
+                            maxScore = currScore;
+                            maxScoreId = count;
+
+                        }
+                        Debug.WriteLine(jObject["data"][count]["id"].ToString());
+                        Debug.WriteLine(jObject["data"][count]["name"].ToString());
+                        Debug.WriteLine(jObject["data"][count]["subType"].ToString());
+                        Debug.WriteLine(currScore);
+                        count += 1;
+
+                        //PlaceName.Append(jObject["data"][count]["name"].ToString());
+                        //Scores.Append(jObject["data"][count]["safetyScores"][fetchScore].ToString());
 
                     }
-                    Debug.WriteLine(jObject["data"][count]["id"].ToString());
-                    Debug.WriteLine(jObject["data"][count]["name"].ToString());
-                    Debug.WriteLine(jObject["data"][count]["subType"].ToString());
-                    Debug.WriteLine(currScore);
-                    count += 1;
 
-                    //PlaceName.Append(jObject["data"][count]["name"].ToString());
-                    //Scores.Append(jObject["data"][count]["safetyScores"][fetchScore].ToString());
+                    //Debug.WriteLine("this is PlaceName: ");
+                    //Debug.WriteLine(PlaceName);
+
+
+                    //Debug.WriteLine("this is Scores: ");
+                    //Debug.WriteLine(Scores);
+
+                    safetyMod = new SafetyViewModel
+                    {
+                        Id = jObject["data"][maxScoreId]["id"].ToString(),
+                        Name = jObject["data"][maxScoreId]["name"].ToString(),
+                        SubType = jObject["data"][maxScoreId]["subType"].ToString(),
+                        SafetyScore = jObject["data"][maxScoreId]["safetyScores"][fetchScore].ToString(),
+                        //Id = jObject["data"][0]["id"].ToString(),
+                        //Name = jObject["data"][0]["name"].ToString(),
+                        //SubType = jObject["data"][0]["subType"].ToString(),
+                        //SafetyScore = jObject["data"][0]["safetyScores"][fetchScore].ToString(),
+                    };
 
                 }
-
-                //Debug.WriteLine("this is PlaceName: ");
-                //Debug.WriteLine(PlaceName);
-
-
-                //Debug.WriteLine("this is Scores: ");
-                //Debug.WriteLine(Scores);
-
-                safetyMod = new SafetyViewModel
-                {
-                    Id = jObject["data"][maxScoreId]["id"].ToString(),
-                    Name = jObject["data"][maxScoreId]["name"].ToString(),
-                    SubType = jObject["data"][maxScoreId]["subType"].ToString(),
-                    SafetyScore = jObject["data"][maxScoreId]["safetyScores"][fetchScore].ToString(),
-                    //Id = jObject["data"][0]["id"].ToString(),
-                    //Name = jObject["data"][0]["name"].ToString(),
-                    //SubType = jObject["data"][0]["subType"].ToString(),
-                    //SafetyScore = jObject["data"][0]["safetyScores"][fetchScore].ToString(),
-                };
-
             }
             else
             {
@@ -113,7 +118,6 @@ namespace SafetyRatings.Controllers
                 token = Get_new_token2(ref token);
             }
             return View(safetyMod);
-
         }
 
         public string Check_token_validity(ref string token)
@@ -166,69 +170,23 @@ namespace SafetyRatings.Controllers
             return token;
         }
 
-        public IRestResponse get_info(ref string token)
+        public IRestResponse get_info(ref string token, ref  string longitude, ref string latitude)
         {
-            var client1 = new RestClient("https://test.api.amadeus.com/v1/safety/safety-rated-locations?");
+            //var baseUrl = new Uri("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=" + latitude + "&longitude=" + longitude + "&radius=2");
+            //Debug.WriteLine("This is baseUrl: ");
+            //Debug.WriteLine(baseUrl);
+            //var client1 = new RestClient(baseUrl);
+            var client1 = new RestClient("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=41.397158&longitude=2.160873&radius=1");
             client1.Timeout = -1;
             var request = new RestRequest(Method.GET);
-            request.AddParameter("latitude", 41.397158);
-            request.AddParameter("longitude", 2.160873);
-            request.AddParameter("radius", "2");
+            //request.AddQueryParameter("latitude", latitude);
+            //request.AddQueryParameter("longitude", longitude);
+            //request.AddQueryParameter("radius", "2");
             request.AddHeader("Authorization", "Bearer " + token);
-            //IRestResponse<List<SafetyViewModel>> response = client1.Execute<List<SafetyViewModel>>(request);
             IRestResponse response = client1.Execute(request);
             Debug.WriteLine(response.Content);
             
             return response;
         }
-
-
-        //private string get_new_token2(ref string token)
-        //{
-        //    throw new NotImplementedException();
-        //}
     }
-        //private static string get_new_token(ref string token)
-        //{
-
-        //    var client3 = new RestClient("https://test.api.amadeus.com/v1/security/oauth2/token");
-        //    client3.Timeout = -1;
-        //    var request3 = new RestRequest(Method.POST);
-        //    request3.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-        //    request3.AddParameter("grant_type", "client_credentials");
-        //    request3.AddParameter("client_id", "JxADxJiWnn4tlRdnKPR5U1duX3kLnCaY");
-        //    request3.AddParameter("client_secret", "wclab2e0JLTdT0bT");
-        //    IRestResponse response3 = client3.Execute(request3);
-        //    Debug.WriteLine(response3.Content);
-
-
-        //    var jObjectToken = JObject.Parse(response3.Content);
-
-        //    Debug.WriteLine("jObjectToken QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
-        //    Debug.WriteLine(jObjectToken);
-
-
-
-        //    foreach (JProperty property in jObjectToken.Properties())
-        //    {
-        //        //Debug.WriteLine("one: ");
-        //        //Debug.WriteLine(property.Name + " - " + property.Value);
-
-        //        if (property.Name == "access_token")
-        //        {
-        //            Debug.WriteLine("actual token? QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ: ");
-
-        //            Debug.WriteLine("one: ");
-        //            Debug.WriteLine(property.Name + " - " + property.Value);
-        //            token = property.Value.ToString();
-
-        //            Debug.WriteLine("two: ");
-        //            Debug.WriteLine(token);
-
-        //        }
-        //        else
-        //            return "invalid";
-        //    }
-        //    return token;
-        //}
 }
